b327808c63f1420391217a1d0d21b6283857df50
Daniel Boyce <danielboyce@Daniels-MacBook-Air.local>
2025-12-08 16:43:01 -0500

    project init

---
+++ university-marketplace-api/src/main/java/com/danielboyce/university_marketplace_api/UniversityMarketplaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.danielboyce.university_marketplace_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityMarketplaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityMarketplaceApiApplication.class, args);
+	}
+
+}
---
+++ university-marketplace-api/src/test/java/com/danielboyce/university_marketplace_api/UniversityMarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielboyce.university_marketplace_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityMarketplaceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
